7244f2e7cf08725855053025da1af3acd120b4ed
abrwhale <abrwhale@gmail.com>
2023-05-10 21:03:46 +0900

    basic_frontend

---
+++ web/frontedn/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    let fronend_message;
+  
+    fetch("http://127.0.0.1:8000/").then((response) => {
+      response.json().then((json) => {
+        fronend_message = json.backend_message;
+      });
+    });
+  </script>
+  
+  <h1>{fronend_message}</h1>
